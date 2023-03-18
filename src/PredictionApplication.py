a1b78668773f49cf5eb16ac74380d8dfdfed477b
vishnu-r-1995 <vishnuvjd@gmail.com>
2023-03-18 14:50:45 +0530

    Add function to get time series data for a ticker

--- src/PredictionApplication.py
+++ src/PredictionApplication.py
@@ -4,3 +4,8 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import plotly.express as px
 import yfinance as yf
+
+def get_dataframe_for_ticker(ticker, period_of_time_series):
+    dataframe = yf.Ticker(ticker).history(period = period_of_time_series).reset_index()
+    return dataframe
+dataframe = get_dataframe_for_ticker('ENV', '5y')
