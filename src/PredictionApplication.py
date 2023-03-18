479d54c45e5349ccd9db162ce5c6ce526f307a1e
vishnu-r-1995 <vishnuvjd@gmail.com>
2023-03-18 17:29:17 +0530

    Display prediction for next 30 days

--- src/PredictionApplication.py
+++ src/PredictionApplication.py
@@ -45,8 +45,8 @@ def Lstm_fallback(x, y):
             loss='mse',
             optimizer=adam_optimizer,
         )
-    es = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=15, restore_best_weights=True)
-    model.fit(x, y, epochs=100, verbose=1, callbacks=[es], validation_split=0.1, batch_size=16)
+    es = tf.keras.callbacks.EarlyStopping(monitor = 'loss', patience = 15, restore_best_weights = True)
+    model.fit(x, y, epochs = 20, verbose = 1, callbacks = [es], validation_split = 0.1, batch_size = 16)
     return model
 
 def Lstm_model(x, y):
@@ -62,11 +62,40 @@ def Lstm_model(x, y):
     regressor.add(Dense(units = 1))
     regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
     
-    es = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=15, restore_best_weights=True)
-    regressor.fit(x, y, epochs = 100, validation_split=0.1, batch_size = 64, verbose=1, callbacks=[es])
+    es = tf.keras.callbacks.EarlyStopping(monitor = 'loss', patience = 15, restore_best_weights = True)
+    regressor.fit(x, y, epochs = 20, validation_split = 0.1, batch_size = 64, verbose = 1, callbacks = [es])
     return regressor
+
+def predict_open(model, dates_for_training, Lstm_x, dataframe_for_training, future, Scaler):
+    forecasting_dates = pd.date_range(list(dates_for_training)[-1], periods = future, freq = '1d').tolist()
+    predicted = model.predict(Lstm_x[-future :])
+    predicted1 = np.repeat(predicted, dataframe_for_training.shape[1], axis = -1)
+    predicted_descaled = Scaler.inverse_transform(predicted1)[:, 0]
+    return predicted_descaled,forecasting_dates
+
+def get_output_dataframe(forecasting_dates, predicted_descaled):
+    dates=[]
+    for i in forecasting_dates:
+        dates.append(i.date())
+    df_final = pd.DataFrame(columns=['Date','Open'])
+    df_final['Date'] = pd.to_datetime(dates)
+    df_final['Open'] = predicted_descaled
+    return df_final
+
+def results(dataframe, lookback, future, Scaler, ticker_name):
+    Lstm_x, Lstm_y, dataframe_for_training, dates_for_training = get_data_for_training(dataframe, lookback, future, Scaler)
+    model = Lstm_model(Lstm_x, Lstm_y)
+    loss = pd.DataFrame(model.history.history)
+    loss.plot()
+    future = 30
+    predicted_descaled, forecasting_dates = predict_open(model, dates_for_training, Lstm_x, dataframe_for_training, future, Scaler)
+    results = get_output_dataframe(forecasting_dates, predicted_descaled)   
+    print(results.head())
+    plt.show()
+    fig = px.area(results, x = "Date", y = "Open", title = ticker_name)
+    fig.update_yaxes(range = [results.Open.min() - 10, results.Open.max() + 10])
+    fig.show()
 
 Scaler = StandardScaler()
 dataframe = get_dataframe_for_ticker('ENV', '5y')
-Lstm_x, Lstm_y, dataframe_for_training, dates_for_training = get_data_for_training(dataframe, 30, 1, Scaler)
-
+results(dataframe, 30, 1, Scaler, 'ENV')
