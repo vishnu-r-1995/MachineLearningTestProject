32bffc2d3771a585de33d5b622d8619ae61fedf3
vishnu-r-1995 <vishnuvjd@gmail.com>
2023-03-19 23:53:32 +0530

    Add many changes and run the project successfully
    The dataset is split into training set and testing set. Training set is then used to do the training and x train and y train are obtained. The testing set is used to obtain x test and y test, which is in turn used for plotting. Prediction is then made using x test and the values obtained are plotted against y test.

--- src/PredictionApplication.py
+++ src/PredictionApplication.py
@@ -12,22 +12,27 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Flatten
 
-def get_dataframe_for_ticker(ticker, period_of_time_series):
-    dataframe = yf.Ticker(ticker).history(period = period_of_time_series).reset_index()
+def get_dataframe_for_ticker(ticker, time_period):
+    dataframe = yf.Ticker(ticker).history(period = time_period).reset_index()
     return dataframe
 
-def get_data_for_training(dataframe, lookback, future, Scale):
-    dates_for_training = pd.to_datetime(dataframe['Date'])
-    dataframe_for_training = dataframe[['Open','High','Low','Close','Volume','Dividends','Stock Splits']]
-    dataframe_for_training = dataframe_for_training.astype(float)
-    dataframe_for_training_scaled = Scaler.fit_transform(dataframe_for_training)
+def prep_data(dataframe, lookback, future, scaler, mode):
+    dataframe_lstm = dataframe[['Open','High','Low','Close','Volume','Dividends','Stock Splits']]
+    dataframe_lstm = dataframe_lstm.astype(float)
+    dataframe_lstm_scaled = scaler.fit_transform(dataframe_lstm)
     
-    x,y = [], []
-    for i in range(lookback, len(dataframe_for_training) - future + 1):
-        x.append(dataframe_for_training_scaled[i - lookback : i, 0 : dataframe_for_training.shape[1]])
-        y.append(dataframe_for_training_scaled[i + future - 1 : i + future, 0])
+    x,y,dates_index = [], [], []
+    for i in range(lookback, len(dataframe_lstm) - future + 1):
+        x.append(dataframe_lstm_scaled[i - lookback : i, 0 : dataframe_lstm.shape[1]])
+        dates_index.append(i + future - 1)
+        if (mode == 'test'):
+            y.append(dataframe_lstm.values[i + future - 1 : i + future, 0])
+        else:
+            y.append(dataframe_lstm_scaled[i + future - 1 : i + future, 0])
+
+    dates_lstm = pd.to_datetime(dataframe['Date'].iloc[dates_index[0] : dates_index[len(dates_index) - 1] + 1])
     #np.array(x) gives a 3D array & np.array(y) gives 2D array
-    return np.array(x), np.array(y), dataframe_for_training, dates_for_training
+    return np.array(x), np.array(y), dataframe_lstm, dates_lstm
     
 def Lstm_fallback(x, y):
     model = Sequential()
@@ -63,18 +68,33 @@ def Lstm_model(x, y):
     regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
     
     es = tf.keras.callbacks.EarlyStopping(monitor = 'loss', patience = 15, restore_best_weights = True)
-    regressor.fit(x, y, epochs = 50, validation_split = 0.1, batch_size = 64, verbose = 1, callbacks = [es])
+    regressor.fit(x, y, epochs = 75, validation_split = 0.1, batch_size = 64, verbose = 1, callbacks = [es])
     return regressor
 
-def predict_open(model, dates_for_training, Lstm_x, dataframe_for_training, future, Scaler):
-    forecasting_dates = pd.date_range(list(dates_for_training)[-1], periods = future, freq = '1d').tolist()
-    predicted = model.predict(Lstm_x[-future :])
-    predicted1 = np.repeat(predicted, dataframe_for_training.shape[1], axis = -1)
+def predict_open(model, dates_for_testing, x_test, dataframe_for_testing, Scaler):
+    forecasting_dates = dates_for_testing.tolist()
+    predicted = model.predict(x_test)
+    predicted1 = np.repeat(predicted, dataframe_for_testing.shape[1], axis = -1)
     predicted_descaled = Scaler.inverse_transform(predicted1)[:, 0]
-    return predicted_descaled,forecasting_dates
+    return predicted_descaled, forecasting_dates
+
+def print_rmse(y_test, predicted_descaled):
+    rmse = np.sqrt(np.mean(y_test - predicted_descaled)**2)
+    print('rmse = ', rmse)
+
+def plot_predicted_vs_real(predicted, real, x_axis, ticker):
+    dataframe_for_plotting = pd.DataFrame({'Predicted':predicted, 'Real':real, 'Date':x_axis})
+    dataframe_for_plotting.set_index('Date', inplace=True)
+    plt.figure(figsize=(16,8))
+    plt.title(ticker)
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Open Price USD', fontsize=18)
+    plt.plot(dataframe_for_plotting[['Predicted','Real']])
+    plt.legend(['Predicted','Real'])
+    plt.show()
 
 def get_output_dataframe(forecasting_dates, predicted_descaled):
-    dates=[]
+    dates = []
     for i in forecasting_dates:
         dates.append(i.date())
     df_final = pd.DataFrame(columns=['Date','Open'])
@@ -82,20 +102,31 @@ def get_output_dataframe(forecasting_dates, predicted_descaled):
     df_final['Open'] = predicted_descaled
     return df_final
 
-def results(dataframe, lookback, future, Scaler, ticker_name):
-    Lstm_x, Lstm_y, dataframe_for_training, dates_for_training = get_data_for_training(dataframe, lookback, future, Scaler)
-    model = Lstm_model(Lstm_x, Lstm_y)
+def results(dataframe_test, dataframe_train, lookback, future, scaler, ticker):
+    x_train, y_train, dataframe_for_training, dates_for_training = prep_data(dataframe_train, lookback, future, scaler, 'train')
+    x_test, y_test, dataframe_for_testing, dates_for_testing = prep_data(dataframe_test, lookback, future, scaler, 'test')
+    
+    model = Lstm_model(x_train, y_train)
     loss = pd.DataFrame(model.history.history)
     loss.plot()
+    
     future = 30
-    predicted_descaled, forecasting_dates = predict_open(model, dates_for_training, Lstm_x, dataframe_for_training, future, Scaler)
+    predicted_descaled, forecasting_dates = predict_open(model, dates_for_testing, x_test, dataframe_for_testing, scaler)
+    
+    print_rmse(y_test.flatten(), np.array(predicted_descaled))
+    plot_predicted_vs_real(np.array(predicted_descaled), y_test.flatten(), forecasting_dates, ticker)
     results = get_output_dataframe(forecasting_dates, predicted_descaled)   
+    
     plt.show()
-    fig = px.area(results, x = "Date", y = "Open", title = ticker_name)
+    fig = px.area(results, x = "Date", y = "Open", title = ticker)
     fig.update_yaxes(range = [results.Open.min() - 10, results.Open.max() + 10])
     fig.show()
 
-Scaler = StandardScaler()
-dataframe = get_dataframe_for_ticker('ENV', '5y')
-dataframe = dataframe.iloc[0 : len(dataframe) - 15, :]
-results(dataframe, 30, 1, Scaler, 'ENV')
+
+scaler = StandardScaler()
+ticker = 'ENV'
+time_period = '10y'
+dataframe = get_dataframe_for_ticker(ticker, time_period)
+dataframe_train = dataframe.iloc[0 : len(dataframe) - 300, :]
+dataframe_test = dataframe.iloc[len(dataframe) - 300 : len(dataframe), :]
+results(dataframe_test, dataframe_train, 30, 1, scaler, ticker)
